aff1e56adc04d2218486d71e9daaebdfb346b262
Rian <rian.ace.2003@gmail.com>
2022-11-01 19:12:23 -0300

    ..

--- assets/js/mainIndex.js
+++ assets/js/mainIndex.js
@@ -87,7 +87,7 @@ function criaContainerTipo(tipo1,tipo2){
 
 //cria card do Pokemon
 function criaPokemon(nome, tipo1, tipo2, num){
-    const areaPokemon = document.querySelector('#areapokemon');
+    const areaPokemon = document.querySelector('.linha');
     areaPokemon.appendChild(criaLink(tipo1,num,criaIMG(tipo1,nome),criaNomeNum(nome,num,criaContainerTipo(tipo1,tipo2))));
 }
 
